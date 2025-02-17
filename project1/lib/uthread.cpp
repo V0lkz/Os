04917132be2a483b0bcf4b28056d282d38772e7c
theduccom <supershredder2003@gmail.com>
2025-02-17 15:55:13 -0600

    Made changes to TCB and uthread_create

--- project1/lib/uthread.cpp
+++ project1/lib/uthread.cpp
@@ -1,21 +1,22 @@
 #include "uthread.h"
-#include "TCB.h"
+
 #include <cassert>
 #include <deque>
-#include <time.h>
+
+#include "TCB.h"
 
 using namespace std;
 
 // Finished queue entry type
 typedef struct finished_queue_entry {
-  TCB *tcb;     // Pointer to TCB
-  void *result; // Pointer to thread result (output)
+    TCB *tcb;        // Pointer to TCB
+    void *result;    // Pointer to thread result (output)
 } finished_queue_entry_t;
 
 // Join queue entry type
 typedef struct join_queue_entry {
-  TCB *tcb;            // Pointer to TCB
-  int waiting_for_tid; // TID this thread is waiting on
+    TCB *tcb;               // Pointer to TCB
+    int waiting_for_tid;    // TID this thread is waiting on
 } join_queue_entry_t;
 
 // You will need to maintain structures to track the state of threads
@@ -31,58 +32,60 @@ typedef struct join_queue_entry {
 // Queues
 static deque<TCB *> ready_queue;
 
+static int tid_num = 0;
+
 // Interrupt Management --------------------------------------------------------
 
 // Start a countdown timer to fire an interrupt
 static void startInterruptTimer() {
-  // TODO
+    // TODO
 }
 
 // Block signals from firing timer interrupt
 static void disableInterrupts() {
-  // TODO
+    // TODO
 }
 
 // Unblock signals to re-enable timer interrupt
 static void enableInterrupts() {
-  // TODO
+    // TODO
 }
 
 // Queue Management ------------------------------------------------------------
 
 // Add TCB to the back of the ready queue
-void addToReadyQueue(TCB *tcb) { ready_queue.push_back(tcb); }
+void addToReadyQueue(TCB *tcb) {
+    ready_queue.push_back(tcb);
+}
 
 // Removes and returns the first TCB on the ready queue
 // NOTE: Assumes at least one thread on the ready queue
 TCB *popFromReadyQueue() {
-  assert(!ready_queue.empty());
+    assert(!ready_queue.empty());
 
-  TCB *ready_queue_head = ready_queue.front();
-  ready_queue.pop_front();
-  return ready_queue_head;
+    TCB *ready_queue_head = ready_queue.front();
+    ready_queue.pop_front();
+    return ready_queue_head;
 }
 
 // Removes the thread specified by the TID provided from the ready queue
 // Returns 0 on success, and -1 on failure (thread not in ready queue)
 int removeFromReadyQueue(int tid) {
-  for (deque<TCB *>::iterator iter = ready_queue.begin();
-       iter != ready_queue.end(); ++iter) {
-    if (tid == (*iter)->getId()) {
-      ready_queue.erase(iter);
-      return 0;
+    for (deque<TCB *>::iterator iter = ready_queue.begin(); iter != ready_queue.end(); ++iter) {
+        if (tid == (*iter)->getId()) {
+            ready_queue.erase(iter);
+            return 0;
+        }
     }
-  }
-
-  // Thread not found
-  return -1;
+    // Thread not found
+    return -1;
 }
 
 // Helper functions ------------------------------------------------------------
 
 // Switch to the next ready thread
 static void switchThreads() {
-  // TODO
+    // TODO
 }
 
 // Library functions -----------------------------------------------------------
@@ -91,67 +94,75 @@ static void switchThreads() {
 // function must do
 
 // Starting point for thread. Calls top-level thread function
-void stub(void *(*start_routine)(void *), void *arg) {}
+void stub(void *(*start_routine)(void *), void *arg) {
+    // Call start routine
+    *(start_routine) (arg);
+    // Call exit if start_routine did not
+    uthread_exit(0);
+}
 
 int uthread_init(int quantum_usecs) {
-  // Initialize any data structures
-  // Setup timer interrupt and handler
-  // Create a thread for the caller (main) thread
+    // Initialize any data structures
+    // Setup timer interrupt and handler
+    // Create a thread for the caller (main) thread
 }
 
 int uthread_create(void *(*start_routine)(void *), void *arg) {
-  // Create a new thread and add it to the ready queue
+    // Create a new thread and add it to the ready queue
+    int tid = tid_num++;    // idk lol
+    TCB *tcb = new TCB(tid, GREEN, start_routine, arg, READY);
+    addToReadyQueue(tcb);
 }
 
 int uthread_join(int tid, void **retval) {
-  // If the thread specified by tid is already terminated, just return
-  // If the thread specified by tid is still running, block until it terminates
-  // Set *retval to be the result of thread if retval != nullptr
+    // If the thread specified by tid is already terminated, just return
+    // If the thread specified by tid is still running, block until it terminates
+    // Set *retval to be the result of thread if retval != nullptr
 }
 
 int uthread_yield(void) {
-  TCB *chosenTCB, *finishTCB;
-  disableInterrupts();
+    TCB *chosenTCB, *finishTCB;
 
-  chosenTCB = popFromReadyQueue();
+    disableInterrupts();
 
-	if(chosenTCB == NULL){
-		return -1;
-	}else{
-		
-	}
+    chosenTCB = popFromReadyQueue();
+    if (chosenTCB == NULL) {
+        // No threads in queue;
+        enableInterrupts();
+        return -1;
+    }
 }
 
 void uthread_exit(void *retval) {
-  // If this is the main thread, exit the program
-  // Move any threads joined on this thread back to the ready queue
-  // Move this thread to the finished queue
+    // If this is the main thread, exit the program
+    // Move any threads joined on this thread back to the ready queue
+    // Move this thread to the finished queue
 }
 
 int uthread_suspend(int tid) {
-  // Move the thread specified by tid from whatever state it is
-  // in to the block queue
+    // Move the thread specified by tid from whatever state it is
+    // in to the block queue
 }
 
 int uthread_resume(int tid) {
-  // Move the thread specified by tid back to the ready queue
+    // Move the thread specified by tid back to the ready queue
 }
 
 int uthread_once(uthread_once_t *once_control, void (*init_routine)(void)) {
-  // Use the once_control structure to determine whether or not to execute
-  // the init_routine
-  // Pay attention to what needs to be accessed and modified in a critical
-  // region (critical meaning interrupts disabled)
+    // Use the once_control structure to determine whether or not to execute
+    // the init_routine
+    // Pay attention to what needs to be accessed and modified in a critical region
+    // (critical meaning interrupts disabled)
 }
 
 int uthread_self() {
-
+    // TODO
 }
 
 int uthread_get_total_quantums() {
-  // TODO
+    // TODO
 }
 
 int uthread_get_quantums(int tid) {
-  // TODO
+    // TODO
 }
