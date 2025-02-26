d9264fdf1a2d7ff5d1974f61e18d453f66851683
theduccom <supershredder2003@gmail.com>
2025-02-25 20:58:05 -0600

    Make new test case

--- project1/tests/test.cpp
+++ project1/tests/test.cpp
@@ -5,6 +5,14 @@
 
 #include "../lib/uthread.h"
 
+void *func5(void *arg) {
+    long i = 0;
+    while (uthread_get_quantums(uthread_self()) < 10) {
+        i++;
+    }
+    return (void *) (i);
+}
+
 void *func2(void *arg) {
     return (void *) ((long) (uthread_self()));
 }
@@ -52,25 +60,43 @@ int main(int argc, char *argv[]) {
 
     uthread_join(tid3, NULL);
 
+    std::cerr << std::endl;
+
     test(4);    // Test suspend and resume
 
     int tid4_1 = uthread_create(func1, NULL);
     int tid4_2 = uthread_create(func1, NULL);
 
     std::cout << "Suspending Thread: " << tid4_1 << std::endl;
-    uthread_suspend(tid4_1); 
+    uthread_suspend(tid4_1);
 
     std::cout << "Joining Thread: " << tid4_2 << std::endl;
     uthread_join(tid4_2, NULL);
 
     std::cout << "Resuming Thread: " << tid4_1 << std::endl;
-    uthread_resume(tid4_1); 
+    uthread_resume(tid4_1);
 
     std::cout << "Joining Thread: " << tid4_1 << std::endl;
-    uthread_join(tid4_1, NULL); 
+    uthread_join(tid4_1, NULL);
 
     std::cout << "Test 4 Completed!" << std::endl;
 
-    
+    std::cerr << std::endl;
+
+    test(5);    // Test interrupt timer
+
+    int tid5[5];
+    for (int i = 0; i < 5; i++) {
+        tid5[i] = uthread_create(func5, NULL);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        void *retval;
+        uthread_join(tid5[i], &retval);
+        std::cout << "Thread: " << tid5[i] << ", Time: " << (long) (retval)
+                  << ", Quantum: " << uthread_get_quantums(tid5[i]) << std::endl;
+    }
+    std::cout << uthread_get_total_quantums() << std::endl;
+
     return 0;
 }
