bc7577c422d011328a05d9876b08532b71205484
Shuo Jiang <77637077+V0lkz@users.noreply.github.com>
2025-02-26 02:03:48 +0000

    added test 4 suspend/resume

--- project1/tests/test.cpp
+++ project1/tests/test.cpp
@@ -48,8 +48,29 @@ int main(int argc, char *argv[]) {
 
     void *ret3;
     int tid3 = uthread_create(func2, &ret3);
-    std::cout << "id: " << tid3 << " ret: " << (long) (ret3);
+    std::cout << "id: " << tid3 << " ret: " << (long) (ret3) << std::endl;
 
-    uthread_exit(NULL);
+    uthread_join(tid3, NULL);
+
+    test(4);    // Test suspend and resume
+
+    int tid4_1 = uthread_create(func1, NULL);
+    int tid4_2 = uthread_create(func1, NULL);
+
+    std::cout << "Suspending Thread: " << tid4_1 << std::endl;
+    uthread_suspend(tid4_1); 
+
+    std::cout << "Joining Thread: " << tid4_2 << std::endl;
+    uthread_join(tid4_2, NULL);
+
+    std::cout << "Resuming Thread: " << tid4_1 << std::endl;
+    uthread_resume(tid4_1); 
+
+    std::cout << "Joining Thread: " << tid4_1 << std::endl;
+    uthread_join(tid4_1, NULL); 
+
+    std::cout << "Test 4 Completed!" << std::endl;
+
+    
     return 0;
 }
