0ea725ca7cb0be6a2f8590114cf308c5c5e6131b
theduccom <supershredder2003@gmail.com>
2025-03-15 16:14:39 -0500

    Edit CondVar.cpp

--- project2/lib/CondVar.cpp
+++ project2/lib/CondVar.cpp
@@ -1,21 +1,40 @@
 #include "CondVar.h"
-#include "uthread_private.h"
+
 #include <cassert>
 
-CondVar::CondVar()
-{
+#include "uthread_private.h"
+
+CondVar::CondVar() {
     // Nothing to do
 }
 
 // Release the lock and block this thread atomically. Thread is woken up when
 // signalled or broadcasted
-void CondVar::wait(Lock &lock)
-{
-    // TODO 
+void CondVar::wait(Lock &lock) {
+    // Ensure thread has lock when calling wait
+    assert(lock.held);
+    disableInterrupts();
+    // Add running thread to condition variable queue
+    running->setState(BLOCK);
+    queue.push(running);
+    // Release the lock while interrupts are disabled
+    lock._unlock();
+    // Switch to another thread
+    switchThreads();
+    // Re-acquire lock
+    lock.lock();
+    // Interrupts already re-enabled
 }
 
 // Wake up a blocked thread if any is waiting
-void CondVar::signal()
-{
-    // TODO
+void CondVar::signal() {
+    disableInterrupts();
+    if (!queue.empty()) {
+        // Remove a thread from queue
+        TCB *next = queue.front();
+        queue.pop();
+        // Signal to run next thread??
+        lock->_signal(next);
+    }
+    enableInterrupts();
 }
