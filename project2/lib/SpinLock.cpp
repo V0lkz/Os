cee8d10fb499422fa596d331961f68ce49f69f13
theduccom <supershredder2003@gmail.com>
2025-03-17 15:14:19 -0500

    Implement SpinLock.cpp

--- project2/lib/SpinLock.cpp
+++ project2/lib/SpinLock.cpp
@@ -1,18 +1,15 @@
 #include "SpinLock.h"
 
-SpinLock::SpinLock()
-{
+SpinLock::SpinLock() {
     // Nothing to do
 }
 
 // Acquire the lock. Spin until the lock is acquired if the lock is already held
-void SpinLock::lock()
-{
-    // TODO
+void SpinLock::lock() {
+    while (atomic_value.test_and_set());
 }
 
 // Unlock the lock
-void SpinLock::unlock()
-{
-    // TODO
+void SpinLock::unlock() {
+    atomic_value.clear();
 }
