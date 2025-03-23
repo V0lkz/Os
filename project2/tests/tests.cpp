d4af4272945a5f5eb6b323043d369a76c0cf6870
theduccom <supershredder2003@gmail.com>
2025-03-23 14:46:21 -0500

    Add test for mutex lock

--- project2/tests/tests.cpp
+++ project2/tests/tests.cpp
@@ -13,33 +13,86 @@ enum tests {
     ASYNC_IO
 };
 
+/* Test 1: Mutex Lock */
+
+#define NUM_THREADS_T1 5
+#define NUM_ITER_T1 5
+
+static volatile short wait = 0;
+static Lock lock_t1;
+
+static int values_t1[NUM_ITER_T1 * NUM_THREADS_T1];
+static int counter_t1 = 0;
+
+void *thread_mutex_lock(void *arg) {
+    int tid = uthread_self();
+    for (int i = 0; i < NUM_ITER_T1; i++) {
+        lock_t1.lock();
+        values_t1[counter_t1++] = tid;
+        // Busy waiting with lock
+        while (++wait)
+            ;
+        lock_t1.unlock();
+    }
+    return (void *) (long) tid;
+}
+
 int test_mutex_lock() {
+    // Create threads
+    int threads[NUM_THREADS_T1];
+    for (int i = 0; i < NUM_THREADS_T1; i++) {
+        uthread_create(thread_mutex_lock, NULL);
+    }
+    // Join threads
+    int total_expected = 0;
+    for (int i = 0; i < NUM_THREADS_T1; i++) {
+        void *temp = nullptr;
+        uthread_join(threads[i], &temp);
+        total_expected += ((long) temp * NUM_ITER_T1);
+    }
+    // Check for correct results
+    if (counter_t1 < NUM_ITER_T1 * NUM_THREADS_T1) {
+        return -1;
+    }
+    int total_actual = 0;
+    for (int i = 0; i < NUM_ITER_T1 * NUM_THREADS_T1; i++) {
+        total_actual += values_t1[i];
+    }
     return 0;
 }
+
+/* Test 2: SpinLock */
 
 int test_spin_lock() {
     return 0;
 }
 
+/* Test 3: Condition Variable */
+
 int test_cond_var() {
     return 0;
 }
 
+/* Test 4: Multiple Condition Variables */
+
 int test_multi_cond_var() {
     return 0;
 }
+
+/* Test 5: Asynchronus I/O */
 
 int test_async_io() {
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc > 3) {
-        std::cerr << "Usage ./tests <testnum> <quantum_usecs>" << std::endl;
+    if (argc != 3) {
+        std::cerr << "Usage ./test <testnum> <quantum_usecs>" << std::endl;
         exit(1);
     }
 
-    const int testnum = atoi(argv[2]);
+    const int testnum = atoi(argv[1]);
+    std::cerr << testnum << std::endl;
     const int quantum_usecs = atoi(argv[2]);
     const bool test_all = (testnum <= 0 ? true : false);
 
@@ -55,6 +108,7 @@ int main(int argc, char *argv[]) {
             std::cerr << "Mutex lock test failed!" << std::endl;
             exit(1);
         }
+        std::cerr << "Mutex lock test passed!" << std::endl;
     }
     if (test_all || testnum == SPIN_LOCK) {
         if (test_spin_lock() != 0) {
@@ -64,19 +118,19 @@ int main(int argc, char *argv[]) {
     }
     if (test_all || testnum == COND_VAR) {
         if (test_cond_var() != 0) {
-            std::cerr << "Cond Var test failed!" << std::endl;
+            std::cerr << "Condition variable test failed!" << std::endl;
             exit(1);
         }
     }
     if (test_all || testnum == MULTI_COND_VAR) {
         if (test_multi_cond_var() != 0) {
-            std::cerr << "Multi Cond Var test failed!" << std::endl;
+            std::cerr << "Multiple condition variables test failed!" << std::endl;
             exit(1);
         }
     }
     if (test_all || testnum == ASYNC_IO) {
         if (test_async_io() != 0) {
-            std::cerr << "Async io test failed!" << std::endl;
+            std::cerr << "Asynchronus i/o test failed!" << std::endl;
             exit(1);
         }
     }
