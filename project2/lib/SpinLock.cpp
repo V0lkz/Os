6ef6508bf78f7bc6e62dfce056a01a56b35acec6
theduccom <supershredder2003@gmail.com>
2025-03-23 17:12:00 -0500

    Add spinlock test

--- project2/lib/SpinLock.cpp
+++ project2/lib/SpinLock.cpp
@@ -1,15 +1,25 @@
 #include "SpinLock.h"
 
+#ifdef DEBUG
+#include "uthread_private.h"
+#define PRINT(format, ...) fprintf(stderr, format, __VA_ARGS__)
+#else
+#define PRINT(fstr, ...)    // DEBUG OFF
+#endif
+
 SpinLock::SpinLock() {
     // Nothing to do
 }
 
 // Acquire the lock. Spin until the lock is acquired if the lock is already held
 void SpinLock::lock() {
+    PRINT("Thread %d aquiring spinlock\n", running->getId());
     while (atomic_value.test_and_set());
+    PRINT("Thread %d acquired spinlock\n", running->getId());
 }
 
 // Unlock the lock
 void SpinLock::unlock() {
+    PRINT("Thread %d released spinlock\n", running->getId());
     atomic_value.clear();
 }
--- project2/tests/tests.cpp
+++ project2/tests/tests.cpp
@@ -3,6 +3,7 @@
 
 #include "../lib/CondVar.h"
 #include "../lib/Lock.h"
+#include "../lib/SpinLock.h"
 #include "../lib/uthread.h"
 
 enum tests {
@@ -13,24 +14,54 @@ enum tests {
     ASYNC_IO
 };
 
+// Busy waiting counter
+static volatile int wait = 0;
+
+/* Testing Setup Functions */
+
+#define NUM_THREADS 5
+
+static void *t_results[NUM_THREADS];
+static int threads[NUM_THREADS];
+
+int testing_setup(void *(*thread_func)(void *), void *args) {
+    // Create threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        threads[i] = uthread_create(thread_func, args);
+        if (threads[i] == -1) {
+            std::cerr << "uthread_create" << std::endl;
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int testing_cleanup() {
+    // Join threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (uthread_join(threads[i], &t_results[i]) != 0) {
+            std::cerr << "uthread_join" << std::endl;
+            return -1;
+        }
+    }
+    return 0;
+}
+
 /* Test 1: Mutex Lock */
 
-#define NUM_THREADS_T1 5
 #define NUM_ITER_T1 5
 
-static volatile int wait = 0;
 static Lock lock_t1;
 
-static int values_t1[NUM_ITER_T1 * NUM_THREADS_T1];
+static int values_t1[NUM_ITER_T1 * NUM_THREADS];
 static int counter_t1 = 0;
 
-void *thread_mutex_lock(void *arg) {
-    (void) arg;
+void *thread_mutex_lock(void *args) {
+    (void) args;
     int tid = uthread_self();
     for (int i = 0; i < NUM_ITER_T1; i++) {
         lock_t1.lock();
         values_t1[counter_t1++] = tid;
-        // Busy waiting with lock
         while (++wait & 0xFFFFFFF);
         lock_t1.unlock();
     }
@@ -39,29 +70,24 @@ void *thread_mutex_lock(void *arg) {
 }
 
 int test_mutex_lock() {
-    // Create threads
-    int threads[NUM_THREADS_T1];
-    for (int i = 0; i < NUM_THREADS_T1; i++) {
-        threads[i] = uthread_create(thread_mutex_lock, NULL);
-        if (threads[i] == -1) {
-            std::cerr << "uthread_create" << std::endl;
-        }
+    // Setup threads
+    if (testing_setup(thread_mutex_lock, nullptr) != 0) {
+        return -1;
     }
     // Join threads
-    int total_expected = 0;
-    for (int i = 0; i < NUM_THREADS_T1; i++) {
-        void *temp = nullptr;
-        if (uthread_join(threads[i], &temp) != 0) {
-            std::cerr << "uthread_join" << std::endl;
-        }
-        total_expected += ((long) temp * NUM_ITER_T1);
+    if (testing_cleanup() != 0) {
+        return -1;
     }
     // Check for correct results
-    if (counter_t1 != NUM_ITER_T1 * NUM_THREADS_T1) {
+    if (counter_t1 != NUM_ITER_T1 * NUM_THREADS) {
         std::cerr << "Counter is incorrect" << std::endl;
         return -1;
     }
-    int total_actual = 0;
+    long total_expected = 0;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        total_expected += (long) t_results[i] * NUM_ITER_T1;
+    }
+    long total_actual = 0;
     for (int i = 0; i < counter_t1; i++) {
         total_actual += values_t1[i];
     }
@@ -74,7 +100,53 @@ int test_mutex_lock() {
 
 /* Test 2: SpinLock */
 
+#define NUM_ITER_T2 5
+
+SpinLock spinlock_t2;
+
+static int values_t2[NUM_ITER_T2 * NUM_THREADS];
+static int counter_t2 = 0;
+
+void *thread_spin_lock(void *args) {
+    (void) args;
+    int tid = uthread_self();
+    for (int i = 0; i < NUM_ITER_T1; i++) {
+        spinlock_t2.lock();
+        values_t2[counter_t2++] = tid;
+        // Busy waiting with lock
+        while (++wait & 0xFFFFFFF);
+        spinlock_t2.unlock();
+    }
+    std::cout << "Thread " << tid << " finished" << std::endl;
+    return (void *) (long) tid;
+}
+
 int test_spin_lock() {
+    // Setup threads
+    if (testing_setup(thread_spin_lock, nullptr) != 0) {
+        return -1;
+    }
+    // Join threads
+    if (testing_cleanup() != 0) {
+        return -1;
+    }
+    // Check for correct results
+    if (counter_t2 != NUM_ITER_T2 * NUM_THREADS) {
+        std::cerr << "Counter is incorrect" << std::endl;
+        return -1;
+    }
+    long total_expected = 0;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        total_expected += (long) t_results[i] * NUM_ITER_T2;
+    }
+    long total_actual = 0;
+    for (int i = 0; i < counter_t2; i++) {
+        total_actual += values_t2[i];
+    }
+    if (total_actual != total_expected) {
+        std::cerr << "Total sum is incorrect" << std::endl;
+        return -1;
+    }
     return 0;
 }
 
@@ -125,6 +197,7 @@ int main(int argc, char *argv[]) {
             std::cerr << "Spinlock test failed!" << std::endl;
             exit(1);
         }
+        std::cerr << "Spinlock test passed!" << std::endl;
     }
     if (test_all || testnum == COND_VAR) {
         if (test_cond_var() != 0) {
