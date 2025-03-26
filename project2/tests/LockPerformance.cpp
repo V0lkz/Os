a5e3acb1979eceee13ad438bb3ac226139eae1ef
theduccom <supershredder2003@gmail.com>
2025-03-26 02:05:09 -0500

    Update lockperformance test

--- project2/tests/LockPerformance.cpp
+++ project2/tests/LockPerformance.cpp
@@ -10,19 +10,19 @@ struct ThreadArgs {
     int inner_loop_size;
 };
 
-Lock global_lock;
-SpinLock spin_lock;
-uint64_t shared_counter = 0;
+static Lock mutex_lock;                         // Mutex Lock
+static SpinLock spin_lock;                      // Spin Lock
+static volatile uint64_t shared_counter = 0;    //  Volatile to prevent compiler optimization
 
-void *critical_section_with_lock(void *arg) {
+void *critical_section_with_mutexlock(void *arg) {
     int iterations_size = ((ThreadArgs *) arg)->num_iterations;
     int loop_size = ((ThreadArgs *) arg)->inner_loop_size;
     for (int i = 0; i < iterations_size; i++) {
-        global_lock.lock();
+        mutex_lock.lock();
         for (int j = 0; j < loop_size; j++) {
             shared_counter++;    // Critical section
         }
-        global_lock.unlock();
+        mutex_lock.unlock();
     }
     return nullptr;
 }
@@ -70,30 +70,40 @@ void run_test(void *(*lock_func)(void *), const std::string &lock_type, int num_
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: ./lockperformance <quantum>" << std::endl;
+    if (argc != 4 && argc != 5) {
+        std::cerr << "Usage: ./lockperformance <num_threads> <iterations> <quantum>" << std::endl;
+        std::cerr << "Usage: ./lockperformance <...> <...> <...> <num_loops>" << std::endl;
         exit(1);
     }
 
+    const int num_threads = atoi(argv[1]);
+    const int iterations = atoi(argv[2]);
+    const int num_loops = (argc == 5 ? atoi(argv[4]) : 10000);
+
     // Initialize thread library
-    uthread_init(atoi(argv[1]));
+    uthread_init(atoi(argv[3]));
 
-    std::cout << "Test 1\n";
-    // run_test(function, (string) locktype, (int) number of threads, (int) number of iterations,
-    // (int) inner loop size);
-    run_test(critical_section_with_lock, "Lock", 10, 100, 1);
+    // run_test(function, (string) locktype, (int) number of threads,
+    // (int) number of iterations, (int) inner loop size);
 
-    // Reset shared counter
-    shared_counter = 0;
-    run_test(critical_section_with_spinlock, "SpinLock", 10, 100, 1);
-
-    std::cout << "Test 2\n";
-    run_test(critical_section_with_lock, "Lock", 10, 100, 10000);
+    std::cout << "==================== Test 1 ====================\n";
+    std::cout << "Testing MutexLock Performance...\n";
+    run_test(critical_section_with_mutexlock, "MutexLock", num_threads, iterations, 1);
 
     // Reset shared counter
     shared_counter = 0;
     std::cout << "Testing SpinLock Performance...\n";
-    run_test(critical_section_with_spinlock, "SpinLock", 10, 100, 10000);
+    run_test(critical_section_with_spinlock, "SpinLock", num_threads, iterations, 1);
+
+    std::cout << "==================== Test 2 ====================\n";
+    shared_counter = 0;
+    std::cout << "Testing MutexLock Performance...\n";
+    run_test(critical_section_with_mutexlock, "MutexLock", num_threads, iterations, num_loops);
+
+    // Reset shared counter
+    shared_counter = 0;
+    std::cout << "Testing SpinLock Performance...\n";
+    run_test(critical_section_with_spinlock, "SpinLock", num_threads, iterations, num_loops);
 
     uthread_exit(nullptr);
     return 0;
