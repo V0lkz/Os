856856aa6631b70626cca2ab1bb6c8d61ec1d3fb
theduccom <supershredder2003@gmail.com>
2025-03-26 23:19:06 -0500

    Update lock performance

--- project2/tests/lock_performance.cpp
+++ project2/tests/lock_performance.cpp
@@ -6,65 +6,71 @@
 #include "../lib/uthread.h"
 
 struct ThreadArgs {
-    int num_iterations;
-    int inner_loop_size;
+    int n_iterations;
+    int n_loops;
+    int workload;
 };
 
 Lock mutex_lock;
 SpinLock spin_lock;
 uint64_t shared_counter = 0;
 
-volatile long x = 0;
+volatile uint64_t x = 0;
 
-void add_workload() {
-    for (int i = 0; i < 100000; ++i) {
+void add_workload(int num_iters) {
+    for (int i = 0; i < num_iters; i++) {
         x += i * i;
     }
 }
 
-void *critical_section_with_mutexlock(void *arg) {
-    int iterations_size = ((ThreadArgs *) arg)->num_iterations;
-    int loop_size = ((ThreadArgs *) arg)->inner_loop_size;
-    // add_workload();
+void *critical_section_with_mutexlock(void *args) {
+    ThreadArgs *params = (ThreadArgs *) args;
+    int iterations_size = params->n_iterations;
+    int loop_size = params->n_loops;
+    int workload = params->workload;
     for (int i = 0; i < iterations_size; i++) {
         mutex_lock.lock();
         for (int j = 0; j < loop_size; j++) {
             shared_counter++;    // Critical section
         }
         mutex_lock.unlock();
+        add_workload(workload);
     }
     return nullptr;
 }
 
-void *critical_section_with_spinlock(void *arg) {
-    int iterations_size = ((ThreadArgs *) arg)->num_iterations;
-    int loop_size = ((ThreadArgs *) arg)->inner_loop_size;
-    // add_workload();
+void *critical_section_with_spinlock(void *args) {
+    ThreadArgs *params = (ThreadArgs *) args;
+    int iterations_size = params->n_iterations;
+    int loop_size = params->n_loops;
+    int workload = params->workload;
     for (int i = 0; i < iterations_size; i++) {
         spin_lock.lock();
         for (int j = 0; j < loop_size; j++) {
             shared_counter++;    // Critical section
         }
         spin_lock.unlock();
+        add_workload(workload);
     }
     return nullptr;
 }
 
-void run_test(void *(*lock_func)(void *), const std::string &lock_type, int num_threads,
-              int num_iterations, int num_inner_loop) {
+void run_test(void *(*lock_func)(void *), const std::string &lock_type, int nthreads, int niters,
+              int nloops, int workload) {
     // Start timer
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    int tids[num_threads];
-    ThreadArgs arg = { num_iterations, num_inner_loop };
-    for (int i = 0; i < num_threads; ++i) {
-        tids[i] = uthread_create(lock_func, &arg);
+    int *tids = (int *) malloc(sizeof(int) * nthreads);
+    ThreadArgs args = { .n_iterations = niters, .n_loops = nloops, .workload = workload };
+
+    for (int i = 0; i < nthreads; ++i) {
+        tids[i] = uthread_create(lock_func, &args);
         if (tids[i] == -1) {
             std::cerr << "uthread_create\n";
         }
     }
 
-    for (int i = 0; i < num_threads; ++i) {
+    for (int i = 0; i < nthreads; ++i) {
         if (uthread_join(tids[i], nullptr) != 0) {
             std::cerr << "uthread_join\n";
         }
@@ -74,38 +80,48 @@ void run_test(void *(*lock_func)(void *), const std::string &lock_type, int num_
     auto end_time = std::chrono::high_resolution_clock::now();
     double duration = std::chrono::duration<double, std::milli>(end_time - start_time).count();
 
-    std::cout << "Lock: " << lock_type << " with " << num_threads << " threads, " << num_iterations
-              << " iterations/thread, " << num_inner_loop << " ops/critical-section"
+    std::cout << lock_type << " with " << nthreads << " threads, " << niters
+              << " iterations/thread, " << nloops << " ops/critical-section"
               << " completed in " << duration << " ms " << std::endl;
+
+    free(tids);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 4 && argc != 5) {
-        std::cerr << "Usage: ./lockperformance <num_threads> <iterations> <quantum>" << std::endl;
-        std::cerr << "Usage: ./lockperformance <...> <...> <...> <num_loops>" << std::endl;
+    if (argc != 6) {
+        std::cerr << "Usage: ./lockperformance <nthreads> <iterations> <nloops> <workload> "
+                     "<quantum>\n";
         exit(1);
     }
 
     const int num_threads = atoi(argv[1]);
     const int iterations = atoi(argv[2]);
-    const int num_loops = (argc == 5 ? atoi(argv[4]) : 10000);
+    const int num_loops = atoi(argv[3]);
+    const int workload = atoi(argv[4]);
 
     // Initialize thread library
-    uthread_init(atoi(argv[3]));
+    uthread_init(atoi(argv[5]));
 
-    // run_test(function, (string) locktype, (int) number of threads,
-    // (int) number of iterations, (int) inner loop size);
+    /**
+     * run_test(
+     *  thread function,
+     *  lock type,
+     *  number of threads,
+     *  number of iterations,
+     *  number of inner loops
+     * )
+     */
 
     std::cout << "==================== Test 1 ====================\n";
 
     std::cout << "Testing MutexLock Performance...\n";
-    run_test(critical_section_with_mutexlock, "MutexLock", num_threads, iterations, 1);
+    run_test(critical_section_with_mutexlock, "MutexLock", num_threads, iterations, 1, 0);
 
     // Reset shared counter
     shared_counter = 0;
 
     std::cout << "Testing SpinLock Performance...\n";
-    run_test(critical_section_with_spinlock, "SpinLock", num_threads, iterations, 1);
+    run_test(critical_section_with_spinlock, "SpinLock", num_threads, iterations, 1, 0);
 
     // Reset shared counter
     shared_counter = 0;
@@ -113,16 +129,15 @@ int main(int argc, char *argv[]) {
     std::cout << "==================== Test 2 ====================\n";
 
     std::cout << "Testing MutexLock Performance...\n";
-    run_test(critical_section_with_mutexlock, "MutexLock", num_threads, iterations, num_loops);
+    run_test(critical_section_with_mutexlock, "MutexLock", num_threads, iterations, num_loops,
+             workload);
 
     // Reset shared counter
     shared_counter = 0;
 
     std::cout << "Testing SpinLock Performance...\n";
-    run_test(critical_section_with_spinlock, "SpinLock", num_threads, iterations, num_loops);
-
-    // Reset shared counter
-    shared_counter = 0;
+    run_test(critical_section_with_spinlock, "SpinLock", num_threads, iterations, num_loops,
+             workload);
 
     uthread_exit(nullptr);
     return 0;
