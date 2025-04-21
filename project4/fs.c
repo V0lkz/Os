b06f9b9a63c15ae0ed6e4bcbe4141e2b53355890
theduccom <supershredder2003@gmail.com>
2025-04-20 19:37:48 -0500

    Edit comments and add fs_unmount

--- project4/fs.c
+++ project4/fs.c
@@ -1,12 +1,13 @@
 #include "fs.h"
-#include "disk.h"
 
 #include <assert.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+#include "disk.h"
 
 #define FS_MAGIC 0xf0f03410
 #define INODES_PER_BLOCK 128
@@ -16,45 +17,41 @@
 // Returns the number of dedicated inode blocks given the disk size in blocks
 #define NUM_INODE_BLOCKS(disk_size_in_blocks) (1 + (disk_size_in_blocks / 10))
 
-struct fs_superblock
-{
-    int magic;        // Magic bytes
-    int nblocks;      // Size of the disk in number of blocks
-    int ninodeblocks; // Number of blocks dedicated to inodes
-    int ninodes;      // Number of dedicated inodes
+struct fs_superblock {
+    int magic;           // Magic bytes
+    int nblocks;         // Size of the disk in number of blocks
+    int ninodeblocks;    // Number of blocks dedicated to inodes
+    int ninodes;         // Number of dedicated inodes
 };
 
-struct fs_inode
-{
-    int isvalid;                    // 1 if valid (in use), 0 otherwise
-    int size;                       // Size of file in bytes
-    int direct[POINTERS_PER_INODE]; // Direct data block numbers (0 if invalid)
-    int indirect;                   // Indirect data block number (0 if invalid)
+struct fs_inode {
+    int isvalid;                       // 1 if valid (in use), 0 otherwise
+    int size;                          // Size of file in bytes
+    int direct[POINTERS_PER_INODE];    // Direct data block numbers (0 if invalid)
+    int indirect;                      // Indirect data block number (0 if invalid)
 };
 
-union fs_block
-{
-    struct fs_superblock super;              // Superblock
-    struct fs_inode inode[INODES_PER_BLOCK]; // Block of inodes
-    int pointers[POINTERS_PER_BLOCK];        // Indirect block of direct data block numbers
-    char data[DISK_BLOCK_SIZE];              // Data block
+union fs_block {
+    struct fs_superblock super;                 // Superblock
+    struct fs_inode inode[INODES_PER_BLOCK];    // Block of inodes
+    int pointers[POINTERS_PER_BLOCK];           // Indirect block of direct data block numbers
+    char data[DISK_BLOCK_SIZE];                 // Data block
 };
 
 static union fs_block superblock;
 
 /**
- * + Freemap should be allocated in fs_mount (via malloc) 
+ * + Freemap should be allocated in fs_mount (via malloc)
  *   based on the number of blocks (nblocks) in your super block
- * 
+ *
  * + freemap can then be indexed like an array for determining whether or
  *   not a block is free... e.g.
  *      if(!freemap[123])
  *          printf("Block 123 is NOT free\n");
-*/
-static int *freemap = 0;
+ */
+static int *freemap = NULL;
 
-void fs_debug()
-{
+void fs_debug() {
     union fs_block block;
 
     disk_read(0, block.data);
@@ -65,98 +62,81 @@ void fs_debug()
     printf("    %d inodes\n", block.super.ninodes);
 }
 
-int fs_format()
-{
-    int nblocks = disk_size();
-    int ninodeblocks = NUM_INODE_BLOCKS(nblocks);
-    int ninodes = ninodeblocks * INODES_PER_BLOCK;
+int fs_format() {
+    const int nblocks = disk_size();
+    const int ninodeblocks = NUM_INODE_BLOCKS(nblocks);
+    const int ninodes = ninodeblocks * INODES_PER_BLOCK;
 
-    //empty the super block
+    // Empty and write the new superblock
     memset(&superblock, 0, sizeof(union fs_block));
-
     superblock.super.magic = FS_MAGIC;
     superblock.super.ninodeblocks = ninodeblocks;
     superblock.super.ninodes = ninodes;
     superblock.super.nblocks = nblocks;
 
-    //write super block to block 0
+    // Write superblock to block 0
     disk_write(0, superblock.data);
 
+    // Empty the inodes
     union fs_block empty_block;
-    memset(&empty_block, 0, sizeof(empty_block));
-    // empty the inodes
-    for (int i = 0; i < ninodeblocks; i++) {
-     disk_write(i + 1, empty_block.data);
+    memset(&empty_block, 0, sizeof(union fs_block));
+    for (int i = 1; i <= ninodeblocks; i++) {
+        disk_write(i, empty_block.data);
     }
 
     return 1;
 }
 
-int fs_mount()
-{
-    //read and get data from super block
-    union fs_block block;
-    disk_read(0, block.data);
-    
+int fs_mount() {
+    // Read data from superblock
+    disk_read(0, superblock.data);
 
-    if(block.super.magic != FS_MAGIC){
+    // Check if magic number is valid
+    if (superblock.super.magic != FS_MAGIC) {
         return 0;
     }
-
-    int nblock = block.super.nblocks;
-
-    if(freemap){
+    // Check if there is alread a free map
+    if (freemap != NULL) {
         free(freemap);
     }
-
-    freemap = malloc(sizeof(int) * nblock);
-
-    if(!freemap){
+    // Allocate a new free map
+    const int nblocks = superblock.super.nblocks;
+    freemap = malloc(sizeof(int) * nblocks);
+    if (freemap == NULL) {
         return 0;
     }
-
-    //set all blocks as free
-    for (int i = 0; i < nblock; i++) {
+    // Set all blocks as free
+    for (int i = 0; i < nblocks; i++) {
         freemap[i] = 0;
     }
-
-    //set super block and inode blocks as used
+    // Set superblock as used
     freemap[0] = 1;
-    for(int i = 1; i <= block.super.ninodeblocks; i++){
-        freemap[i] = 0;
-    }
-
-
-
     return 1;
 }
 
-int fs_unmount()
-{
-    return 0;
+int fs_unmount() {
+    if (freemap != NULL) {
+        free(freemap);
+    }
+    return 1;
 }
 
-int fs_create()
-{
+int fs_create() {
     return -1;
 }
 
-int fs_delete(int inumber)
-{
+int fs_delete(int inumber) {
     return 0;
 }
 
-int fs_getsize(int inumber)
-{
+int fs_getsize(int inumber) {
     return -1;
 }
 
-int fs_read(int inumber, char *data, int length, int offset)
-{
+int fs_read(int inumber, char *data, int length, int offset) {
     return 0;
 }
 
-int fs_write(int inumber, const char *data, int length, int offset)
-{
+int fs_write(int inumber, const char *data, int length, int offset) {
     return 0;
 }
